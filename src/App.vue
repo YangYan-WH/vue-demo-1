e1bbbd8c07c8d571d215417ef4380c1d6a57f164
yangyan <15927288912@126.com>
2020-03-22 20:21:15 +0800

    居中

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class='wrapper'>
-    <div class="row">
-      <Cell v-on:click='onClickCell(0,$event)' v-bind:n="n"/>
-      <Cell v-on:click='onClickCell(1,$event)' v-bind:n="n"/>
-      <Cell v-on:click='onClickCell(2,$event)' v-bind:n="n"/>
+  <div class="wrapper">
+    <div class="board">
+      <div class="row">
+        <Cell v-on:click="onClickCell(0,$event)" v-bind:n="n" />
+        <Cell v-on:click="onClickCell(1,$event)" v-bind:n="n" />
+        <Cell v-on:click="onClickCell(2,$event)" v-bind:n="n" />
+      </div>
+      <div class="row">
+        <Cell v-on:click="onClickCell(3,$event)" v-bind:n="n" />
+        <Cell v-on:click="onClickCell(4,$event)" v-bind:n="n" />
+        <Cell v-on:click="onClickCell(5,$event)" v-bind:n="n" />
+      </div>
+      <div class="row">
+        <Cell v-on:click="onClickCell(6,$event)" v-bind:n="n" />
+        <Cell v-on:click="onClickCell(7,$event)" v-bind:n="n" />
+        <Cell v-on:click="onClickCell(8,$event)" v-bind:n="n" />
+      </div>
+      
     </div>
-    <div class="row">
-      <Cell v-on:click='onClickCell(3,$event)' v-bind:n="n"/>
-      <Cell v-on:click='onClickCell(4,$event)' v-bind:n="n"/>
-      <Cell v-on:click='onClickCell(5,$event)' v-bind:n="n"/>
-    </div>
-    <div class="row">
-      <Cell v-on:click='onClickCell(6,$event)' v-bind:n="n"/>
-      <Cell v-on:click='onClickCell(7,$event)' v-bind:n="n"/>
-      <Cell v-on:click='onClickCell(8,$event)' v-bind:n="n"/>
-    </div>
-    <div class="result" v-bind:class="{success: !!result}" >
-      {{result?`${result} 获得胜利`:'胜负未分'}}
-    </div>
+    <div class="result" v-bind:class="{success: !!result}">{{result?`${result} 获得胜利`:'胜负未分'}}</div>
   </div>
 </template>
 
@@ -25,81 +26,83 @@
 import Cell from "./Cell";
 
 export default {
-  data(){
+  data() {
     return {
-      n:0,
-      map:[
-        [null,null,null],
-        [null,null,null],
-        [null,null,null]
+      n: 0,
+      map: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
       ],
-      result:null,
-    }
+      result: null
+    };
   },
   components: {
     Cell
   },
-  methods:{
-    onClickCell(i, text){
-      console.log('app')
-      console.log(text)
-      console.log(i)
-      
-      this.map[Math.floor(i / 3)][i%3] = text
-      this.n = this.n + 1
-      this.tell(text)
+  methods: {
+    onClickCell(i, text) {
+      console.log("app");
+      console.log(text);
+      console.log(i);
+
+      this.map[Math.floor(i / 3)][i % 3] = text;
+      this.n = this.n + 1;
+      this.tell(text);
     },
-    tell(text){
-      let map = this.map
-      let result = this.result
-      console.log(map)
-      for(let i = 0;i<2;i++){
-        if(map[i][0] && map[i][0] === map[i][1] && map[i][1] === map[i][2]){
-          this.result = text
+    tell(text) {
+      let map = this.map;
+      let result = this.result;
+      console.log(map);
+      for (let i = 0; i < 2; i++) {
+        if (map[i][0] && map[i][0] === map[i][1] && map[i][1] === map[i][2]) {
+          this.result = text;
         }
       }
 
-      for(let j = 0;j<2;j++){
-        if(map[0][j] && map[0][j] === map[1][j] && map[1][j] === map[2][j]){
-          this.result = text
+      for (let j = 0; j < 2; j++) {
+        if (map[0][j] && map[0][j] === map[1][j] && map[1][j] === map[2][j]) {
+          this.result = text;
         }
       }
 
-      if(map[0][0] && map[0][0]===map[1][1] && map[1][1] === map[2][2]){
-        this.result = text
+      if (map[0][0] && map[0][0] === map[1][1] && map[1][1] === map[2][2]) {
+        this.result = text;
       }
-      if(map[2][0] && map[2][0]=== map[1][1] && map[1][1] === map[0][2]){
-        this.result = text
+      if (map[2][0] && map[2][0] === map[1][1] && map[1][1] === map[0][2]) {
+        this.result = text;
       }
-
     }
   }
 };
 </script>
 
 <style>
-body{
+body {
   height: 100vh;
-  background-color: #2a80eb;
+  background-color: #0a70efba;
 }
-  .wrapper{
-    text-align: center;
-    margin:  100px 0;
-  }
-  .row{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-  }
-  .row > div{
-    background-color: white;
-  }
-  .result{
-    font-size: 32px;
-    color:#f7f9fa;
-  }
-  .success{
-    color: #1cad70;
-  }
+.wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.row > div {
+  background-color: white;
+}
+.result {
+  font-size: 32px;
+  color: #f7f9fa;
+}
+.success {
+  color: #eb4646;
+}
 </style>
